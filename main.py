cafe1a82d0fab014c80c0022c01ab4ead25f848f
Andre Bossard <anbossar@microsoft.com>
2022-03-01 17:28:07 +0100

    it's all the same

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+"""
+This is an example of higher order functions and how they compare e.g. to class based structures.
+
+Essentially it's the same concept:
+- you have a constructor (e.g. __init__ or the create function)
+- you have a concealed place for dependencies or state (e.g. members or closure)
+- you return something that combines/uses the inside state
+
+
+"""
+
+
 class Adder:
+    """
+    This is a class based structure.
+    """
     def __init__(self, a):
+        """
+        This is the constructor.
+        :param a:
+        """
         self.a = a
 
     def add(self, b):
@@ -7,6 +26,11 @@ class Adder:
 
 
 def create_adder(a):
+    """
+    This is a function based structure. and similar to the constructor above
+    :param a:
+    :return:
+    """
     return lambda b: a + b
 
 
@@ -24,3 +48,5 @@ if __name__ == '__main__':
     print(adder2(20))
     adder3 = create_adder2(10)
     print(adder3(20))
+
+
